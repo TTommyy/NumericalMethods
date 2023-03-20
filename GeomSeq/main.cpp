098a478a248361f674d9bcf085847c7c65150b84
Tomasz Koczar <tomekkoczar@gmail.com>
2023-03-20 23:15:25 +0100

    init

--- GeomSeq/main.cpp
+++ GeomSeq/main.cpp
@@ -1,24 +1,33 @@
 // Tomasz Koczar
 
 #include <iostream>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <cmath>
 
 using std::cout;
 using std::cin;
 
-using Triple = std::tuple<float, float, float>;
-using Pair = std::pair<float, float>;
-
-inline Triple extract(float product, float sum)
+inline void extract(const float product, const float sum, std::ostream& o)
 {
-
+    float x;
+    float q;
+    o << x << " " << x * q << " " << x * q * q << "\n";
 }
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
+
+    cout.precision(10);
+    cout << std::scientific;
+
     int NUBMER_OF_SEQ;
     cin >> NUBMER_OF_SEQ;
 
+    float product, sum;
+
+    for (int i = 0; i < NUBMER_OF_SEQ; i++)
+    {
+        cin >> product >> sum;
+        extract(product, sum, cout);
+    }
 }
