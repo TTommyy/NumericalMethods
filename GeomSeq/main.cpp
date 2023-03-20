199c65524cef1c031a79d63c9fed44dc1c027310
Tomasz Koczar <tomekkoczar@gmail.com>
2023-03-20 20:37:40 +0100

    Init

---
+++ GeomSeq/main.cpp
@@ -0,0 +1,24 @@
+// Tomasz Koczar
+
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using std::cout;
+using std::cin;
+
+using Triple = std::tuple<float, float, float>;
+using Pair = std::pair<float, float>;
+
+inline Triple extract(float product, float sum)
+{
+
+}
+
+int main()
+{
+    int NUBMER_OF_SEQ;
+    cin >> NUBMER_OF_SEQ;
+
+}
