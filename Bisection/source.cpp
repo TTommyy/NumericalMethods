4d5a88f272028c64fe7997cf19af48af03df4954
Tomasz Koczar <tomekkoczar@gmail.com>
2023-03-30 17:27:11 +0100

    B final

--- Bisection/source.cpp
+++ Bisection/source.cpp
@@ -2,10 +2,15 @@
 
 #include <cmath>
 
+const double BOUND = 0.001;
+const double FORCE_SWITCH = 0.8;
+const double DIVIDER = 10.0;
+
 double secant(
     double (*f)(double),
     const double a,
     const double b,
+    const double fa,
     const int M,
     const double eps,
     const double delta)
@@ -14,9 +19,10 @@ double secant(
     double x1 = b;
     double x2;
     double len;
-    double fx0 = f(x0);
+    double fx0 = fa;
     double fx1;
-    for (int i = 0; i < M; i++)
+
+    for (int i = 0; i < M ; i++)
     {
         fx1 = f(x1);
         if (std::abs(fx1) <= eps)
@@ -24,7 +30,8 @@ double secant(
             return x1;
         }
 
-        x2 = x1 - fx1 * ((x1 - x0) / (fx1 - fx0));
+        x2 = x1 - fx1 * (x1 - x0) / (fx1 - fx0);
+
         len = std::abs(x2 - x1);
         if (len <= delta)
         {
@@ -42,20 +49,26 @@ double bisect(
     double (*f)(double),
     const double a0,
     const double b0,
+    const double fa0,
     const int M,
     const double eps,
     const double delta)
 {
     double a = a0;
     double b = b0;
-    double len;
     double c;
-    double fa;
+    double len;
+    double fa = fa0;
     double fc;
-    for (int i = 0; i < M; i++)
+
+    int Mbi = M * FORCE_SWITCH;
+    int Msec = M - Mbi;
+
+    for (int i = 0; i < Mbi; i++)
     {
-        len = (b - a) / 2;
         c = (a + b) / 2;
+
+        len = (b - a) / 2;
         if (len <= delta)
         {
             return c;
@@ -76,8 +89,13 @@ double bisect(
         {
             b = c;
         }
+
+        if (std::abs(fc) <= BOUND)
+        {
+            return secant(f, a, b, fa, M - i - 1, eps, delta);
+        }
     }
-    return c;
+    return secant(f, a, b, fa, Msec, eps, delta);
 }
 
 double findZero(
@@ -91,9 +109,19 @@ double findZero(
     const double fa = f(a);
     const double fb = f(b);
     M = M - 2;
+
+    if (std::abs(fa) <= eps)
+    {
+        return a;
+    }
+    if (std::abs(fb) <= eps)
+    {
+        return b;
+    }
+
     if (fa * fb > 0)
     {
-        return secant(f, a, b, M, eps, delta);
+        return secant(f, a, b, fa, M, eps/DIVIDER, delta/DIVIDER);
     }
-    return bisect(f, a, b, M, eps, delta);
+    return bisect(f, a, b, fa, M, eps/DIVIDER, delta/DIVIDER);
 }
